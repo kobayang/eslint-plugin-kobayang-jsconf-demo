f9444c21ff58031f251ab0ba19094a8839424a25
Naoki Kobayashi <kobayashi@wantedly.com>
2021-11-26 16:10:06 +0900

    Add @typescript-eslint/parser

--- src/rules/__tests__/sample-rule.test.ts
+++ src/rules/__tests__/sample-rule.test.ts
@@ -1,7 +1,9 @@
 import { RuleTester } from "eslint";
 import { RULE, RULE_NAME } from "../sample-rule";
 
-const tester = new RuleTester();
+const tester = new RuleTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+});
 
 tester.run(RULE_NAME, RULE, {
   valid: [],
