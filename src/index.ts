08aef1bad19af0363059134914c96174a623f87a
Naoki Kobayashi <kobayashi@wantedly.com>
2021-11-26 15:37:12 +0900

    Add rule to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as DEMO_RULE from "./rules/sample-rule";
+import * as NO_PARENT_IMPORT_IN_COMMONS from "./rules/no-parent-import-in-commons";
 
 export const rules = {
   [DEMO_RULE.RULE_NAME]: DEMO_RULE.RULE,
+  [NO_PARENT_IMPORT_IN_COMMONS.RULE_NAME]: NO_PARENT_IMPORT_IN_COMMONS.RULE,
 };
