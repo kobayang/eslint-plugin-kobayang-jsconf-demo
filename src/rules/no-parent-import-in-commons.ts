64816cc7d9a5272ecb8225df71f1988765c2a999
Naoki Kobayashi <kobayashi@wantedly.com>
2021-11-26 15:25:38 +0900

    Add utility func

--- src/rules/no-parent-import-in-commons.ts
+++ src/rules/no-parent-import-in-commons.ts
@@ -1,5 +1,26 @@
 import { Rule } from "eslint";
 
+/**
+ * filename が commons ディレクトリ内にあるかどうか
+ * @param filename パスも含んだ filename.
+ * @returns
+ */
+const isFileInCommonsDir = (filename: string): boolean => {
+  const regexp = new RegExp(/\/commons\//)
+  return regexp.test(filename);
+};
+
+/**
+ * path が 親ディレクトリを含んでいるかどうか
+ * @param importSource import元のファイルまたはmodule.
+ * @returns
+ */
+const includesParentPath = (importSource: string): boolean => {
+  // `../` の文字列を含んでいるかどうかで簡易的に判定する
+  const regexp = new RegExp(/\.\.\//);
+  return regexp.test(importSource);
+}
+
 export const RULE_NAME = "no-parent-import-in-commons";
 
 export const RULE: Rule.RuleModule = {
